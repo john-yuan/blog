89955571e14db84e3f39776f4107882790fd81cc
John Yuan <John.YCQ@outlook.com>
2020-07-31 12:14:15 +0800

    feat: update the shell script to create post file

--- _scripts/createPost.sh
+++ _scripts/createPost.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-readonly __DIR__=$(cd $(dirname $0) && pwd)
-readonly POSTS="${__DIR__}/_posts"
+readonly CURRENT_DIR=$(cd $(dirname $0) && pwd)
+readonly ROOT_DIR=$(cd $CURRENT_DIR/.. && pwd)
+readonly POSTS_DIR="${ROOT_DIR}/_posts"
 
-if [ "$1" == ""  ]
+if [ "$1" == "" ]
 then
     echo "Please enter the file name"
     echo "Usage: ${0} <filename>"
@@ -13,7 +14,7 @@ fi
 readonly DATE=$(date "+%Y-%m-%d")
 readonly FILENAME="${DATE}-${1}.md"
 
-cd $POSTS
+cd $POSTS_DIR
 touch $FILENAME
 
 if [ "$(cat $FILENAME)" == "" ]
@@ -24,7 +25,13 @@ then
     echo "category:" >> $FILENAME
     echo "tags:" >> $FILENAME
     echo "---" >> $FILENAME
+    echo "" >> $FILENAME
+    echo "<!--more-->" >> $FILENAME
+    echo
+    echo "File created: _posts/$FILENAME"
+    echo
+else
+    echo
+    echo "_posts/$FILENAME is not empty, exit without touching it"
+    echo
 fi
-
-vim $FILENAME
-
